756d367dbbe1bfa4c5facbdd9acb5aad1cf5ae0b
Breno Farias <xxdestructionxx1900@gmail.com>
2023-07-31 22:52:46 -0300

    excluding .env proccess to github

--- index.js
+++ index.js
@@ -22,15 +22,19 @@ const commands = [
   
   const rest = new REST({ version: '10' }).setToken(process.env.TOKEN); // token do bot
   
-  try {
-    console.log('Started refreshing application (/) commands.');
+  (async function(){
+    try {
+        console.log('Started refreshing application (/) commands.');
+      
+        await rest.put(Routes.applicationCommands(client_id), { body: commands });
+      
+        console.log('Successfully reloaded application (/) commands.');
+      } catch (error) {
+        console.error(error);
+      }
+  })();
   
-    rest.put(Routes.applicationCommands(client_id), { body: commands });
   
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
 
   // Configurações básicas de inicialização do bot
   bot.on('ready', () => {
