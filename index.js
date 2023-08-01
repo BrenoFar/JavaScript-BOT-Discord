41cb2bee97a2a64dac603d67377860f1b5933f83
Breno Farias <xxdestructionxx1900@gmail.com>
2023-07-31 22:43:27 -0300

    Creating BOT Configs

---
+++ index.js
@@ -0,0 +1,48 @@
+const { Client, GatewayIntentBits, Collection, REST, Routes } = require("discord.js");
+const { Guilds, GuildMessages, GuildVoiceStates, GuildIntegrations } = GatewayIntentBits;
+const { DisTube } = require("distube");
+const { client_id } = require("./configuration/config.json")
+
+const bot = new Client({ // Instancia o bot
+    intents: [Guilds, GuildMessages, GuildVoiceStates, GuildIntegrations],
+});
+
+
+// para acessar o token do .env 
+require("dotenv").config();
+
+//registrar os comandos SLASH
+
+const commands = [
+    {
+      name: 'ping',
+      description: 'Replies with Pong!',
+    },
+  ];
+  
+  const rest = new REST({ version: '10' }).setToken(process.env.TOKEN); // token do bot
+  
+  try {
+    console.log('Started refreshing application (/) commands.');
+  
+    rest.put(Routes.applicationCommands(client_id), { body: commands });
+  
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+
+  // Configurações básicas de inicialização do bot
+  bot.on('ready', () => {
+    console.log(`Logged in as ${bot.user.tag}!`);
+  });
+  
+  bot.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+  
+    if (interaction.commandName === 'ping') {
+      await interaction.reply('Pong!');
+    }
+  });
+  
+  bot.login(process.env.TOKEN);
